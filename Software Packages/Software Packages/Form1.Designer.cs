73b5e79ff6d86be6b751cd38093f71e6f2e93e07
Ahmed Talima <70567575+ahmednagi22@users.noreply.github.com>
2022-05-06 19:42:34 +0200

    update some changes to BRESENHAM

--- Software Packages/Software Packages/Form1.Designer.cs
+++ Software Packages/Software Packages/Form1.Designer.cs
@@ -64,10 +64,9 @@
             // DrawLineDDA
             // 
             this.DrawLineDDA.BackColor = System.Drawing.Color.DodgerBlue;
-            this.DrawLineDDA.Location = new System.Drawing.Point(41, 130);
-            this.DrawLineDDA.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.DrawLineDDA.Location = new System.Drawing.Point(36, 98);
             this.DrawLineDDA.Name = "DrawLineDDA";
-            this.DrawLineDDA.Size = new System.Drawing.Size(204, 41);
+            this.DrawLineDDA.Size = new System.Drawing.Size(178, 31);
             this.DrawLineDDA.TabIndex = 0;
             this.DrawLineDDA.Text = "Draw Line With DDA";
             this.DrawLineDDA.UseVisualStyleBackColor = false;
@@ -76,18 +75,18 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 41);
+            this.label1.Location = new System.Drawing.Point(14, 31);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 20);
+            this.label1.Size = new System.Drawing.Size(47, 15);
             this.label1.TabIndex = 1;
             this.label1.Text = "Point1 :";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 97);
+            this.label2.Location = new System.Drawing.Point(14, 73);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(57, 20);
+            this.label2.Size = new System.Drawing.Size(47, 15);
             this.label2.TabIndex = 2;
             this.label2.Text = "Point2 :";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -97,8 +96,7 @@
             this.panel1.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
             this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.panel1.Location = new System.Drawing.Point(295, -2);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Location = new System.Drawing.Point(258, -2);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(908, 565);
             this.panel1.TabIndex = 3;
@@ -106,165 +104,156 @@
             // 
             // X1DDA
             // 
-            this.X1DDA.Location = new System.Drawing.Point(77, 37);
-            this.X1DDA.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.X1DDA.Location = new System.Drawing.Point(67, 28);
             this.X1DDA.Name = "X1DDA";
-            this.X1DDA.Size = new System.Drawing.Size(67, 27);
+            this.X1DDA.Size = new System.Drawing.Size(59, 23);
             this.X1DDA.TabIndex = 4;
             // 
             // Y1DDA
             // 
-            this.Y1DDA.Location = new System.Drawing.Point(166, 37);
-            this.Y1DDA.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Y1DDA.Location = new System.Drawing.Point(145, 28);
             this.Y1DDA.Name = "Y1DDA";
-            this.Y1DDA.Size = new System.Drawing.Size(78, 27);
+            this.Y1DDA.Size = new System.Drawing.Size(69, 23);
             this.Y1DDA.TabIndex = 5;
             // 
             // X2DDA
             // 
-            this.X2DDA.Location = new System.Drawing.Point(77, 97);
-            this.X2DDA.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.X2DDA.Location = new System.Drawing.Point(67, 73);
             this.X2DDA.Name = "X2DDA";
-            this.X2DDA.Size = new System.Drawing.Size(67, 27);
+            this.X2DDA.Size = new System.Drawing.Size(59, 23);
             this.X2DDA.TabIndex = 6;
             // 
             // Y2DDA
             // 
-            this.Y2DDA.Location = new System.Drawing.Point(166, 97);
-            this.Y2DDA.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Y2DDA.Location = new System.Drawing.Point(145, 73);
             this.Y2DDA.Name = "Y2DDA";
-            this.Y2DDA.Size = new System.Drawing.Size(78, 27);
+            this.Y2DDA.Size = new System.Drawing.Size(69, 23);
             this.Y2DDA.TabIndex = 7;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(77, 13);
+            this.label3.Location = new System.Drawing.Point(67, 10);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(26, 20);
+            this.label3.Size = new System.Drawing.Size(20, 15);
             this.label3.TabIndex = 8;
             this.label3.Text = "X1";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(166, 13);
+            this.label4.Location = new System.Drawing.Point(145, 10);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(25, 20);
+            this.label4.Size = new System.Drawing.Size(20, 15);
             this.label4.TabIndex = 9;
             this.label4.Text = "Y1";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(77, 73);
+            this.label5.Location = new System.Drawing.Point(67, 55);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(26, 20);
+            this.label5.Size = new System.Drawing.Size(20, 15);
             this.label5.TabIndex = 10;
             this.label5.Text = "X2";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(166, 73);
+            this.label6.Location = new System.Drawing.Point(145, 55);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(25, 20);
+            this.label6.Size = new System.Drawing.Size(20, 15);
             this.label6.TabIndex = 11;
             this.label6.Text = "Y2";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(166, 251);
+            this.label7.Location = new System.Drawing.Point(145, 188);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(25, 20);
+            this.label7.Size = new System.Drawing.Size(20, 15);
             this.label7.TabIndex = 22;
             this.label7.Text = "Y2";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(77, 251);
+            this.label8.Location = new System.Drawing.Point(67, 188);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(26, 20);
+            this.label8.Size = new System.Drawing.Size(20, 15);
             this.label8.TabIndex = 21;
             this.label8.Text = "X2";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(194, 191);
+            this.label9.Location = new System.Drawing.Point(170, 143);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(25, 20);
+            this.label9.Size = new System.Drawing.Size(20, 15);
             this.label9.TabIndex = 20;
             this.label9.Text = "Y1";
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(105, 191);
+            this.label10.Location = new System.Drawing.Point(92, 143);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(26, 20);
+            this.label10.Size = new System.Drawing.Size(20, 15);
             this.label10.TabIndex = 19;
             this.label10.Text = "X1";
             // 
             // Y2Bresenham
             // 
-            this.Y2Bresenham.Location = new System.Drawing.Point(166, 275);
-            this.Y2Bresenham.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Y2Bresenham.Location = new System.Drawing.Point(145, 206);
             this.Y2Bresenham.Name = "Y2Bresenham";
-            this.Y2Bresenham.Size = new System.Drawing.Size(78, 27);
+            this.Y2Bresenham.Size = new System.Drawing.Size(69, 23);
             this.Y2Bresenham.TabIndex = 18;
             // 
             // X2Bresenham
             // 
-            this.X2Bresenham.Location = new System.Drawing.Point(77, 275);
-            this.X2Bresenham.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.X2Bresenham.Location = new System.Drawing.Point(67, 206);
             this.X2Bresenham.Name = "X2Bresenham";
-            this.X2Bresenham.Size = new System.Drawing.Size(67, 27);
+            this.X2Bresenham.Size = new System.Drawing.Size(59, 23);
             this.X2Bresenham.TabIndex = 17;
             // 
             // Y1Bresenham
             // 
-            this.Y1Bresenham.Location = new System.Drawing.Point(166, 215);
-            this.Y1Bresenham.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Y1Bresenham.Location = new System.Drawing.Point(145, 161);
             this.Y1Bresenham.Name = "Y1Bresenham";
-            this.Y1Bresenham.Size = new System.Drawing.Size(78, 27);
+            this.Y1Bresenham.Size = new System.Drawing.Size(69, 23);
             this.Y1Bresenham.TabIndex = 16;
             // 
             // X1Bresenham
             // 
-            this.X1Bresenham.Location = new System.Drawing.Point(77, 215);
-            this.X1Bresenham.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.X1Bresenham.Location = new System.Drawing.Point(67, 161);
             this.X1Bresenham.Name = "X1Bresenham";
-            this.X1Bresenham.Size = new System.Drawing.Size(67, 27);
+            this.X1Bresenham.Size = new System.Drawing.Size(59, 23);
             this.X1Bresenham.TabIndex = 15;
             // 
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(16, 275);
+            this.label11.Location = new System.Drawing.Point(14, 206);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(57, 20);
+            this.label11.Size = new System.Drawing.Size(47, 15);
             this.label11.TabIndex = 14;
             this.label11.Text = "Point2 :";
             // 
             // label12
             // 
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(16, 219);
+            this.label12.Location = new System.Drawing.Point(14, 164);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(57, 20);
+            this.label12.Size = new System.Drawing.Size(47, 15);
             this.label12.TabIndex = 13;
             this.label12.Text = "Point1 :";
             // 
             // DrawLineBresenham
             // 
             this.DrawLineBresenham.BackColor = System.Drawing.Color.DodgerBlue;
-            this.DrawLineBresenham.Location = new System.Drawing.Point(41, 313);
-            this.DrawLineBresenham.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.DrawLineBresenham.Location = new System.Drawing.Point(36, 235);
             this.DrawLineBresenham.Name = "DrawLineBresenham";
-            this.DrawLineBresenham.Size = new System.Drawing.Size(204, 40);
+            this.DrawLineBresenham.Size = new System.Drawing.Size(178, 30);
             this.DrawLineBresenham.TabIndex = 23;
             this.DrawLineBresenham.Text = "Draw Line With Bresenham";
             this.DrawLineBresenham.UseVisualStyleBackColor = false;
@@ -273,60 +262,58 @@
             // label13
             // 
             this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(130, 427);
+            this.label13.Location = new System.Drawing.Point(114, 320);
             this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(53, 20);
+            this.label13.Size = new System.Drawing.Size(42, 15);
             this.label13.TabIndex = 33;
             this.label13.Text = "Radius";
             // 
             // label15
             // 
             this.label15.AutoSize = true;
-            this.label15.Location = new System.Drawing.Point(191, 365);
+            this.label15.Location = new System.Drawing.Point(167, 274);
             this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(17, 20);
+            this.label15.Size = new System.Drawing.Size(14, 15);
             this.label15.TabIndex = 31;
             this.label15.Text = "Y";
             // 
             // X
             // 
             this.X.AutoSize = true;
-            this.X.Location = new System.Drawing.Point(102, 365);
+            this.X.Location = new System.Drawing.Point(89, 274);
             this.X.Name = "X";
-            this.X.Size = new System.Drawing.Size(18, 20);
+            this.X.Size = new System.Drawing.Size(14, 15);
             this.X.TabIndex = 30;
             this.X.Text = "X";
             // 
             // Radius_Circle
             // 
-            this.Radius_Circle.Location = new System.Drawing.Point(73, 451);
-            this.Radius_Circle.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Radius_Circle.Location = new System.Drawing.Point(64, 338);
             this.Radius_Circle.Name = "Radius_Circle";
-            this.Radius_Circle.Size = new System.Drawing.Size(167, 27);
+            this.Radius_Circle.Size = new System.Drawing.Size(147, 23);
             this.Radius_Circle.TabIndex = 29;
             // 
             // Y_Circle
             // 
-            this.Y_Circle.Location = new System.Drawing.Point(175, 391);
-            this.Y_Circle.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Y_Circle.Location = new System.Drawing.Point(153, 293);
             this.Y_Circle.Name = "Y_Circle";
-            this.Y_Circle.Size = new System.Drawing.Size(78, 27);
+            this.Y_Circle.Size = new System.Drawing.Size(69, 23);
             this.Y_Circle.TabIndex = 27;
             // 
             // X_Circle
             // 
-            this.X_Circle.Location = new System.Drawing.Point(73, 391);
-            this.X_Circle.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.X_Circle.Location = new System.Drawing.Point(64, 293);
             this.X_Circle.Name = "X_Circle";
-            this.X_Circle.Size = new System.Drawing.Size(67, 27);
+            this.X_Circle.Size = new System.Drawing.Size(59, 23);
             this.X_Circle.TabIndex = 26;
             // 
             // Draw_circle
             // 
             this.Draw_circle.BackColor = System.Drawing.Color.DodgerBlue;
-            this.Draw_circle.Location = new System.Drawing.Point(41, 485);
+            this.Draw_circle.Location = new System.Drawing.Point(36, 364);
+            this.Draw_circle.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Draw_circle.Name = "Draw_circle";
-            this.Draw_circle.Size = new System.Drawing.Size(190, 44);
+            this.Draw_circle.Size = new System.Drawing.Size(166, 33);
             this.Draw_circle.TabIndex = 0;
             this.Draw_circle.Text = "Draw Circle";
             this.Draw_circle.UseVisualStyleBackColor = false;
@@ -334,10 +321,10 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.ClientSize = new System.Drawing.Size(1281, 685);
+            this.ClientSize = new System.Drawing.Size(1283, 693);
             this.Controls.Add(this.Draw_circle);
             this.Controls.Add(this.label13);
             this.Controls.Add(this.label15);
@@ -368,7 +355,6 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.DrawLineDDA);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
